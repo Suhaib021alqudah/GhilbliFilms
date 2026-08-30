a89668cee283a2724415bee88bccd752d7f6075c
SuhaibAlqudah <alqudahsuhaib@gmail.com>
2026-08-30 11:31:42 +0300

    Test

--- GhibliFilms/ViewModel/Service/MockGhibliService.swift
+++ GhibliFilms/ViewModel/Service/MockGhibliService.swift
@@ -7,10 +7,41 @@
 
 import Foundation
 
-struct MockGhibliService : GhibliService {
-    func fetchFilms() async throws -> [Film] {
-        return [] 
+struct MockGhibliService: GhibliService {
+
+    //MARK: - Mock Sample Data
+
+    private struct SampleData: Decodable {
+        let films: [Film]
+        let people: [Person]
     }
 
-    
+    private func loadSampleData() async throws -> SampleData {
+        guard
+            let url = Bundle.main.url(
+                forResource:
+
+                    "SampleData",
+                withExtension: "json"
+            ) 
+        else {
+            throw APIError.invalidURL
+        }
+        do {
+            let data = try Data(contentsOf: url)
+            return try JSONDecoder().decode(SampleData.self, from: data)
+
+        } catch let error as DecodingError {
+            throw APIError.decoding(error)
+        } catch let error as URLError {
+            throw APIError.networkError(error)
+        }
+
+    }
+
+    func fetchFilms() async throws -> [Film] {
+        let data = try await loadSampleData()
+        return data.films
+    }
+
 }
